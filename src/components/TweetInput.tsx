116be6079f78efa3d7650a05bf2c3b050c86eec9
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-10 13:55:01 +0900

    ログインユーザーavatar + tweet投稿機能

---
+++ src/components/TweetInput.tsx
@@ -0,0 +1,82 @@
+import React, { ChangeEvent, useState } from "react";
+import styles from "./TweetInput.module.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth, storage, db } from "../firebase";
+import { Avatar, Button, IconButton } from "@material-ui/core";
+import firebae from "firebase/app";
+import { timeStamp } from "console";
+
+const TweetInput = () => {
+  const user = useSelector(selectUser);
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const [tweetMsg, setTweetMsg] = useState("");
+
+  // コールバック関数
+  const onChangeImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // msgは必須　imgはどちらでも
+    if (tweetImage) {
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + tweetImage.name;
+      const uploardTweetImg = storage.ref(`images/${fileName}`).put(tweetImage);
+      uploardTweetImg.on(
+        firebae.storage.TaskEvent.STATE_CHANGED,
+        () => {},
+        (err) => {
+          alert(err.message);
+        },
+        async () => {
+          await storage
+            .ref("images")
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMsg,
+                timeStamp: firebae.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              });
+            });
+        }
+      );
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMsg,
+        timeStamp: firebae.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setTweetImage(null);
+    setTweetMsg("");
+  };
+
+  return (
+    <div>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut();
+        }}
+      />
+    </div>
+  );
+};
+
+export default TweetInput;
